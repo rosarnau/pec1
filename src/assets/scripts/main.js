7e3787ffd4ef898129226f9dbb1248bce4279c8c
rosarnau <116513309+rosarnau@users.noreply.github.com>
2023-04-09 20:04:03 +0200

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,12 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+
+/**
+ * Write any other JavaScript below
+ */
